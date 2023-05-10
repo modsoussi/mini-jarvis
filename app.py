e7da8da9802d93cbbce3e83bc026c16f5d331484
modsoussi <mohameddsoussi@gmail.com>
2023-05-10 19:03:19 -0400

    fails most of the time, but sometimes magnificently succeeds

--- app.py
+++ app.py
@@ -17,11 +17,12 @@ if __name__ == "__main__":
       system_prompt="""You are a helpful AI agent called MARKI. Given an input from the user, your job is to 
       generate an action to perform next to address the user's need, taking into account any previous actions and results.
       When the Context is empty or None, simply ignore it. 
-      Actions must accomplish a singular goal. You must be as specific as you possibly can.
+      Do not repeat actions that have already been done.
+      Actions must accomplish a singular goal. You must be as specific as you possibly can, and do not combine actions.
       For every action you generate, prefix with it with one of the following action types:
-      [google-search]: when a google search is required to proceed
-      [web-browse]: when browsing is needed to proceed
-      [ask-for-info]: when you need to ask the user to provide missing data
+      [google-search]: when you need to perform a google search
+      [web-browse]: when you need to browse the web
+      [ask-for-info]: when key data is missing and you need to ask the user for it to proceed
       [final-answer]: when you have an aswer to the user's input from the context.
       [other]: when the action is none of the above
 
@@ -57,12 +58,12 @@ if __name__ == "__main__":
     action_ord = 0
     while True:
       results = {}
-      print(f"***\nContext:\n{context}\n***")
+      # print(f"***\nContext:\n{context}\n***")
       
       action = action_agent.get_completion(user_input, context)
       action_ord += 1
       results[f"Action #{action_ord}"] = action
-      # print(action)
+      print(action)
       parts = action.strip("\n").split("\n")
       
       m = re.search(r"(?s)(?:\w\s)*:?\s*(\[.*?\])\s*(.*)", parts[0])
@@ -77,7 +78,7 @@ if __name__ == "__main__":
             query = m.group(2)
             print(f"Searching on Google: {query}")
             search_results = kernel.web.search(query)
-            results["Search Results"] = search_results
+            results[f"Action #{action_ord} Results"] = search_results
         elif action_type == "[web-browse]":
           method = None
           url = None
@@ -115,20 +116,20 @@ if __name__ == "__main__":
                     "value": inp.get("value")
                   } for inp in form.find_all("input") if inp["type"] != "hidden"]
               } for form in soup.find_all("form")]
-            results["Web Result"] = [text, forms]
+            results[f"Action #{action_ord} Results"] = [text, forms]
           elif method == "POST":
             print(f"POST {url} with params: {params}")
             post_result = kernel.web.post(url, data=params)
             if type(post_result) is BeautifulSoup:
               post_result = re.sub(r"[ \r\t\n]+", " ", post_result.body.text)
             
-            results["Web Results"] = post_result
+            results[f"Action #{action_ord} Results"] = post_result
         elif action_type == "[ask-for-info]":
           m = re.search(r"(?s)(?:\w\s)*:?\s*(\[.*?\])\s*(.*)", parts[1])
           if m.group(1) == "[prompt]":
             prompt = m.group(2)
             print(f"{prompt}")
-            context["Action Result"] = input("> ")
+            results[f"Action #{action_ord} Results"] = input("> ")
         elif action_type == "[final-answer]":
           print(parts)
           exit(0)
