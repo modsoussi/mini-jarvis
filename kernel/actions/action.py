cef9d98c17632eb7bb6545b0a64cee7993535523
modsoussi <mohameddsoussi@gmail.com>
2023-06-24 17:58:41 -0400

    better logging of click events

--- kernel/actions/action.py
+++ kernel/actions/action.py
@@ -1,4 +1,5 @@
 from kernel.browser import Browser
+import urllib
 import json
 
 ACTION_TYPE_ASK_FOR_INFO = "[ask-for-info]"
@@ -45,7 +46,10 @@ class Action:
       return self.prompt
     elif self.action_type == ACTION_TYPE_WEB_BROWSE:
       if self.method == "GET":
-        return f"Clicked on {self.url}"
+        desc = f"Clicked on {self.url}"
+        if self.params is not None:
+          desc = desc + f"/?{urllib.parse.urlencode(self.params)}"
+        return desc
       elif self.method == "POST":
         return f"Submitting form on {self.url}"
     elif self.action_type == ACTION_TYPE_SEARCH:
